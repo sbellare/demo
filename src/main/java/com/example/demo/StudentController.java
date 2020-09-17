63de08d9030b53fae28b74a8fab358a3fa0c1366
sbellare <hellsbells@gmail.com>
2020-09-17 02:44:35 -0500

    Init

---
+++ src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+	@GetMapping("/students")
+	public String students() {
+		return "Student1, Student2, Student3, Student4, Student5, Student6, Student7";
+	}
+}
